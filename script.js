a520f5deb17bea2ce4c7e494b21563e3f1d1b226
unknown <you@example.com>
2020-07-06 14:17:41 +0530

    animation

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 {   let button=document.getElementById("searchButton");
     button.addEventListener('click',function(){
-    let listitems=document.getElementById("list");
-    let input=document.getElementById("searchInput");
-    let error= document.getElementById("error");
-    var results=document.getElementById('result');
-
-    let list=document.getElementById("list");
-    if(listitems){
-        
-    listitems.remove();
-    }
-    let text=input.value;
-    console.log(text);
+       let listitems=document.getElementById("list");
+       let input=document.getElementById("searchInput");
+       let error= document.getElementById("error");
+       var results=document.getElementById('result');
+       error.style.visibility='hidden';
+       input.style.borderColor='lightgray';
     
-    let xhrhttp=new XMLHttpRequest();
-    xhrhttp.onload=function(){
+        let list=document.getElementById("list");
+        if(listitems){
+        
+           listitems.remove();
+            }
+       let text=input.value;
+       console.log(text);
+    
+       let xhrhttp=new XMLHttpRequest();
+       xhrhttp.onload=function(){
         
          let json=  xhrhttp.response;
          
@@ -27,36 +29,47 @@
                 // alert("Search a valid Name");
                 return;
             }
-            var list=document.createElement('ul');
+         var list=document.createElement('ul');
             
-            for(var result in data.results){
+         for(var result in data.results){
 
                  
                 var listitem=document.createElement('li');
                 var favbutton  =document.createElement('button');
+                var image=document.createElement('img');
+                var span=document.createElement('span');
+                image.className='images';
+                image.src=data.results[result].image.url;
+                console.log(data.results[result].image.url);
+
+
                 list.id='list';
                 results.appendChild(list);
                 
                 favbutton.innerText='Add To Fav';
                 favbutton.className='favbtn';
                 listitem.className='listitem';
-                listitem.remove();
-              
-                listitem.innerText=data.results[result].name;
+                
+                
+                span.innerText=data.results[result].name;
+                listitem.appendChild(image);
+                listitem.appendChild(span);
                 listitem.appendChild(favbutton);
                 list.appendChild(listitem);
                 console.log(data.results[result].name);
             }
          
   
-      }
+        }
 
     
 
-    xhrhttp.open('get','https://www.superheroapi.com/api.php/2018504438283292/search/'+text,true);
-    xhrhttp.send(); 
+       xhrhttp.open('get','https://www.superheroapi.com/api.php/2018504438283292/search/'+text,true);
+       xhrhttp.send(); 
   
-    })  
+      })  
+
+              
 
 
 
